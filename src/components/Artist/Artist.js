4c6480ca38bed289255f603330d7ed7e0f882c38
MohammedAnsary <a.n.s.a.r.y@hotmail.com>
2017-05-07 23:14:42 +0200

    Data fetching now works

---
+++ src/components/Artist/Artist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from 'axios';
+import './artist.css';
+import {Link} from 'react-router-dom';
+
+const Artist = (props) => {
+
+    return (
+            <div>Test</div>
+    	)
+}
+export default Artist;
--- src/components/GridList/GridList.js
+++ src/components/GridList/GridList.js
@@ -7,18 +7,17 @@ import {Link} from 'react-router-dom';
 const GridList = (props) => {
 
     return (
-                    <div className = "grid">
-                        {props.list.map(function(item, i){
-                            let hasImage = item.images.length;
-                            return (
-                                <Link to="/" className="grid__item" key={i}>
-                                <div className="album" style={{backgroundImage: `url(${(hasImage)?item.images[0].url : defaultAlbum})`}}></div>
-                                <h5 className="songName">{item.name}</h5>
-                                {/*<img src={(hasImage)?item.images[0].url : ''}/>*/}
-                                </Link>
-                                )
-                        })}
-                    </div>
+            <div className = "grid">
+                {props.list.map(function(item, i){
+                    let hasImage = item.images.length;
+                    return (
+                        <Link to={`/${props.type}/${item.id}`} className="grid__item" key={i}>
+                            <div className="grid__item__image" style={{backgroundImage: `url(${(hasImage)?item.images[0].url : defaultAlbum})`}}/>
+                            <h5 className="grid__item__text">{item.name}</h5>
+                        </Link>
+                    )
+                })}
+            </div>
     	)
 }
 export default GridList;
