dc16c4628d9d9dcfe80cab8ada039359b3f2faf6
Omar Essam <o.essameldin@gmail.com>
2017-05-08 03:18:01 +0200

    album page without tracks

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -4,6 +4,7 @@ import GridList from '../GridList/GridList'
 import './page.css';
 import {Link} from 'react-router-dom';
 import Artist from '../Artist/Artist';
+import Album from '../Album/Album';
 
 const baseurl = "https://api.spotify.com/v1/";
 
@@ -177,7 +178,9 @@ class Page extends React.Component{
             case 'albums':
                 return (
                     <div className="main">
-
+                            {this.state.album &&
+                                <Album album={this.state.album} tracks={this.state.trackList} />
+                            }
                     </div>
                 )
             case 'artists':
